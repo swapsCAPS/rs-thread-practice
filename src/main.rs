24f5ee371d3fd28621bc5f81965d3a2c109bc048
swapsCAPS <verstegen.daan@gmail.com>
2020-05-16 15:52:48 +0200

    Implemented struct with thread handle and start fn
    - TODO stop function
    - More research

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,3 @@
-use std::thread;
-use std::time::Duration;
-use std::sync::{Arc, RwLock };
-
-struct Items {
-    fr: RwLock<u8>,
-    fl: RwLock<u8>,
-}
-
-fn spawn_thread (a_ref: &Arc<RwLock<u8>>) {
-    fn writer (a_ref: Arc<RwLock<u8>>) {
-        let mut go_back = false;
-        loop {
-            // NOTE that the lock is held until the end of the scope
-            // So if we sleep at the end. We will hold a write lock so no one can read for that
-            // duration.
-            // We can use a drop() to release the lock.
-
-            let mut w = a_ref.write().unwrap();
-
-
-            if *w >= 255 {
-                go_back = true;
-            }
-
-            if *w == 0 {
-                go_back = false;
-            }
-
-            if go_back {
-                *w -= 1;
-            } else {
-                *w += 1;
-            }
-
-            // NOTE drop() does nothing speacial. The function just takes ownership and thus the
-            // memory is freed after its scope ends.
-            drop(w);
-            // thread::sleep(Duration::from_millis(1));
-        }
-
-    }
-
-    let clone = Arc::clone(&a_ref);
-
-    thread::spawn(move || {
-        writer(clone)
-    });
-}
-
 // TODO
 // - I'd like to send a signal to a thread so it stops its loop.
 // - It should have a start and stop function.
@@ -55,7 +5,10 @@ fn spawn_thread (a_ref: &Arc<RwLock<u8>>) {
 //   NOTE
 //     it appears that it's quite difficult to have a struct "wrap" a thread handle...
 //     I'm getting lifetime issues and I have not found an elegant notation yet.
+//     This post has a comment detailing why this cannot work:
 //     https://stackoverflow.com/questions/42043823/design-help-threading-within-a-struct
+//     "The signature of ::spawn takes a closure with a 'static lifetime"
+//     I'm trying to pass in &self which then of course also should be 'static.
 //     https://www.reddit.com/r/rust/comments/7iuzy8/thread_living_as_long_as_a_struct/
 //     The first post lists an alternative, but I'm not a fan. I also want to mutate values of self
 //     withing the thread func, so I'm not sure it would even work.
@@ -73,31 +26,84 @@ fn spawn_thread (a_ref: &Arc<RwLock<u8>>) {
 //   I want to be able to say <thread_name>.stop().
 //   There is no way to forEach over a struct. So what is a nice approach?
 
+// NOTE
+// We could use this approach to build some sort of movement engine
+// Setting servo positions with the read values.
+// The threads will be in control of movement speed, positions, etc.
+// The main loop will just "set" the PWM signals for all servos continuously
+
+use std::thread;
+use std::time::Duration;
+use std::sync::{Arc, RwLock, Mutex };
+
+type ArcMutex<T> = Arc<Mutex<T>>;
+
+struct YoloThread {
+    alive: ArcMutex<bool>,
+    go_back: ArcMutex<bool>,
+    value: Arc<RwLock<u8>>,
+    handle: Option<thread::JoinHandle<()>>,
+}
+
+impl YoloThread {
+    pub fn new() -> Self {
+        YoloThread {
+            alive: Arc::new(Mutex::new(false)),
+            go_back: Arc::new(Mutex::new(false)),
+            value: Arc::new(RwLock::new(0)),
+            handle: None
+        }
+    }
+
+    pub fn start(&mut self) {
+
+        // NOTE I think what the post mentions is that you could put all this in a struct wrapped
+        // by ArcMut and then lock it
+        let value = self.value.clone();
+        let go_back = self.go_back.clone();
+
+        self.handle = Some( thread::spawn(move || {
+            loop {
+                // NOTE that the lock is held until the end of the scope
+                // So if we sleep at the end. We will hold a write lock so no one can read for that
+                // duration.
+                // We can use a drop() to release the lock.
+
+                let mut w = value.write().unwrap();
+
+                let mut gb = go_back.lock().unwrap();
+
+                if *w >= 255 {
+                    *gb = true;
+                }
+
+                if *w == 0 {
+                    *gb = false;
+                }
+
+                if *gb {
+                    *w -= 1;
+                } else {
+                    *w += 1;
+                }
+
+                // NOTE drop() does nothing speacial. The function just takes ownership and thus the
+                // memory is freed after its scope ends.
+                drop(w);
+                // thread::sleep(Duration::from_millis(1));
+            }
+        }));
+    }
+}
+
 fn main() {
-    let items = Items {
-        fr: RwLock::new(0),
-        fl: RwLock::new(0),
-    };
-
-    let ar_items = Arc::new(&items);
-
-    let arc_fr = Arc::new(items.fr);
-    let arc_fl = Arc::new(items.fl);
-
-    spawn_thread(&arc_fr);
-    spawn_thread(&arc_fl);
+    let mut yolo_thread = YoloThread::new();
+    yolo_thread.start();
 
     loop {
-        let fr = arc_fr.read().unwrap();
-        let fl = arc_fl.read().unwrap();
+        let val = yolo_thread.value.read().unwrap();
+        println!("val {}", val);
 
-        // NOTE
-        // We could use this approach to build some sort of movement engine
-        // Setting servo positions with the read values.
-        // The threads will be in control of movement speed, positions, etc.
-        // The main loop will just "set" the PWM signals for all servos continuously
-
-        println!("fr {} fl {}", fr, fl);
         thread::sleep(Duration::from_millis(1));
     }
 }
